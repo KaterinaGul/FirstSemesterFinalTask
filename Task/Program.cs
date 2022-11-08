0e8fadd386e88f9fee0097a48875022f78f6ba38
katrin <slavnova.katya@gmail.com>
2022-11-08 14:25:47 +0300

    Написали программу

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array1 = new string[4] { "hello", "2", "world", ":-)" };
+string[] array2 = new string[array1.Length];
+void Array2Check(string[] array1, string[] array2)
+{
+    int n = 3;
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= n)
+        {
+            array2[j] = array1[i];
+            j++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Array2Check(array1, array2);
+PrintArray(array2);
